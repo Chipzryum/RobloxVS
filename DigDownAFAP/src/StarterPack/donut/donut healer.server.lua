4914647e5fba64a22c61feed92f39ee82ebc79c0
Chipzryum <Chipzryum@outlook.com>
2024-08-18 19:00:46 +1000

    Reworked Healing
    reworked healing donut

--- DigDownAFAP/src/StarterPack/donut/donut healer.server.lua
+++ DigDownAFAP/src/StarterPack/donut/donut healer.server.lua
@@ -2,6 +2,32 @@ local donut = script.Parent
 
 donut.Activated:Connect(function()
 	donut.Parent.Humanoid.Health = donut.Parent.Humanoid.Health + 10
-	print("Healed yourself")
-	wait(1)
+	print("Healed the player amongus")
+	wait(1) 
+end)
+local debounce = false
+
+donut.Activated:Connect(function()
+	if not debounce then
+		debounce = true
+		donut.Parent.Humanoid.Health = donut.Parent.Humanoid.Health + 10
+		print("Healed the player amongus")
+		wait(1)
+		debounce = false
+	end
+end)
+local cooldown = 5 -- Set the cooldown time in seconds
+local debounce = false
+
+donut.Activated:Connect(function()
+	if not debounce then
+		debounce = true
+		donut.Parent.Humanoid.Health = donut.Parent.Humanoid.Health + 10
+		print("Healed the player")
+		wait(1)
+		print("Cooldown started")
+		wait(cooldown)
+		print("Cooldown ended")
+		debounce = false
+	end
 end)
